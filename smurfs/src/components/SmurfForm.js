02e593eed2ed2900d5a061a32228f32bdd40d304
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-08-16 13:30:04 -0500

    form still WIP; switching to work on axios call

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
 
+
 export const SmurfForm = () => {
+       
        const [smurf, setSmurf] = useState({})
+       const handleChange = event => {
+              const newSmurf = {name:smurf.name, 
+                                age:smurf.age,
+                                   height: smurf.height,
+                                   id: Date.now()}
+                                   
+              setSmurf(newSmurf)
+       }
        const handleSubmit = event => {
-              event.preventDefault();
               
        }
+       console.log('smurf',smurf)
        return(
         <form onSubmit={handleSubmit}>
         <input type='text'
                name='name'
                placeholder='name' 
-               value={value}/>
+               value={smurf.name}
+               onChange={handleChange}/>
         <input type='int'
                name='age' 
                placeholder='age' 
-               value={age}/>
+               value={smurf.age}
+               onChange={handleChange}/>
         <input type='text'
                name='height' 
                placeholder='height (in cm)' 
-               value={height}/>
+               value={smurf.height}
+               onChange={handleChange}/>
         <button type='submit'>Add Smurf</button>
         
     </form>
