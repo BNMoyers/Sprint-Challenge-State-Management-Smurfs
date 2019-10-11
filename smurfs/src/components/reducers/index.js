c5014c1cac3b76e8667611ed4dbc8e3fd52da123
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-11 10:37:58 -0500

    set up actions and reducers for get request

--- smurfs/src/components/reducers/index.js
+++ smurfs/src/components/reducers/index.js
@@ -1,3 +1,5 @@
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from '../actions'
+
 const initialState = {
     smurfs:[
         
@@ -15,6 +17,26 @@ const initialState = {
 
 export const reducer = (state=initialState, action) => {
     switch(action.type){
+       case START_FETCHING:
+           return {
+               ...state,
+               isFetching: true,
+               error: ''
+           };
+        case FETCH_SUCCESS:
+            return{
+                ...state,
+                isFetching: false,
+                error: '',
+                smurfs: action.payload
+            };
+        case FETCH_FAILURE:
+            return{
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+       
         default:
             return state;
     }
