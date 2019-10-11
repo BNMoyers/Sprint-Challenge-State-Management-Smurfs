c43510a529fcbbd4a41ee32f05a10cd908e2ba0d
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-11 11:24:35 -0500

    form and post request working

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import SmurfList from './SmurfList.js'
+import SmurfList from './SmurfList'
+import NewSmurf from './NewSmurf'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <SmurfList />
+        <NewSmurf />
       </div>
     );
   }
--- smurfs/src/components/NewSmurf.js
+++ smurfs/src/components/NewSmurf.js
@@ -1,6 +1,52 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-const NewSmurf = () => {
-    
-}
+import { sendSmurfs } from "./actions";
+
+const NewSmurf = props => {
+  const [smurf, setSmurf] = useState({ name: "", age: "", height: "", id: "" });
+
+  const handleChange = e => {
+    setSmurf({ ...smurf, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    const newbie = {
+      name: smurf.name,
+      age: parseInt(smurf.age),
+      height: smurf.height,
+      id: Date.now()
+    };
+
+    props.sendSmurfs(smurf);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        placeholder="name"
+        value={smurf.name}
+        onChange={handleChange}
+      />
+      <input
+        type='int'
+        name="age"
+        placeholder="age"
+        value={smurf.age}
+        onChange={handleChange}
+      />
+      <input
+        type="int"
+        name="height"
+        placeholder="height (in cm)"
+        value={smurf.height}
+        onChange={handleChange}
+      />
+      <button type='submit'>Add Smurf</button>
+    </form>
+  );
+};
+
+export default connect(null, {sendSmurfs})(NewSmurf)
